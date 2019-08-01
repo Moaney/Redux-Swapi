480bddda16b9244669798bf739842b507b7a6500
Moaney <ladonnamoney@gmail.com>
2019-08-01 16:34:37 -0400

    added export to action types, added export on reducer file

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,4 +1,9 @@
-import /* we need our action types here*/ "../actions";
+import {
+  FETCH_CHAR_START,
+  FETCH_CHAR_SUCCESS,
+  FETCH_CHAR_FAILED
+} from "../actions";
+
 const initialState = {
   characters: [],
   fetching: false,
@@ -6,11 +11,32 @@ const initialState = {
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
+  console.log('charsReducer', action);
   switch (action.type) {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCH_CHAR_START:
+      return{
+        ...state,
+        fetching: true,
+        error: null
+      };
+    case FETCH_CHAR_SUCCESS:
+       return{
+         ...state,
+         characters: action.payload,
+         fetching: false,
+         error: null
+       };
+    case FETCH_CHAR_FAILED:
+       return{
+         ...state,
+         fetching: false,
+         error: action.payload
+       }   
     default:
       return state;
   }
 };
+export default charsReducer;
